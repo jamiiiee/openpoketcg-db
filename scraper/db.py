f7a055850ba7fbf745d6ab801467065dec57e8d8
jami-3 <jamienewbigging@gmail.com>
2025-06-02 22:14:48 +0100

    db is accessible

--- scraper/db.py
+++ scraper/db.py
@@ -1,33 +1,31 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+from contextlib import contextmanager
 
 load_dotenv()
 
 
-def get_db_connection():
+@contextmanager
+def db_cursor():
     try:
-        connection = psycopg2.connect(
+        conn = psycopg2.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
             host=os.getenv("DB_HOST"),
             port=os.getenv("DB_PORT"),
             dbname=os.getenv("DB_NAME"),
         )
-        print("Connection successful!")
-
-        cursor = connection.cursor()
-
-        cursor.execute("SELECT NOW();")
-        result = cursor.fetchone()
-        print("Current Time:", result)
-        print(result)
-
-        cursor.close()
-        connection.close()
-
     except Exception as e:
-        print(f"Error connecting to the database: {e}")
+        raise RuntimeError(f"Failed to connect to the database: {e}") from e
 
-
-get_db_connection()
+    try:
+        cur = conn.cursor()
+        yield cur
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        cur.close()
+        conn.close()
