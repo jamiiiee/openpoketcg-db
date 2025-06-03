8c0279fc9fbfae0a1c365fc808f87b55593d9e4a
jami-3 <jamienewbigging@gmail.com>
2025-06-03 13:00:06 +0100

    added first two db functions to rebuild_db

--- scripts/rebuild_db.py
+++ scripts/rebuild_db.py
@@ -1,5 +1,25 @@
-# drop sets and cards
-# get eras
+from scraper.db import db_cursor
+
+SQL_TRUNCATE = """
+TRUNCATE TABLE cards, sets RESTART IDENTITY CASCADE;
+"""
+
+SQL_SELECT_ERA_NAMES = """
+SELECT name FROM eras;
+"""
+
+
+def truncate_tables():
+    with db_cursor() as cur:
+        cur.execute(SQL_TRUNCATE)
+
+
+def get_era_names():
+    with db_cursor() as cur:
+        cur.execute(SQL_SELECT_ERA_NAMES)
+        return [row[0] for row in cur.fetchall()]
+
+
 # download(eras)
 # data = scraper()
 # set sets and cards
