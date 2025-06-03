b7f1221fcc44d34f7133fdb27be5e2c81face8bd
jami-3 <jamienewbigging@gmail.com>
2025-06-03 20:41:52 +0100

    fixed update_db sql string

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -1,10 +1,14 @@
 from scraper.download import update_html
 from scraper.db import db_cursor
 
+SQL_SELECT_ONGOING_ERAS = """
+SELECT name FROM eras WHERE ongoing = TRUE;
+"""
+
 
 def get_ongoing_era_names():
     with db_cursor() as cur:
-        cur.execute("SELECT name FROM eras WHERE ongoing = TRUE;")
+        cur.execute(SQL_SELECT_ONGOING_ERAS)
         return [row[0] for row in cur.fetchall()]
 
 
