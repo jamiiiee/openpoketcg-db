259a578aa6802b850ad585f3a627c1d8bd20b857
jami-3 <jamienewbigging@gmail.com>
2025-06-03 14:33:12 +0100

    added first db function to update_db.py

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -1,4 +1,12 @@
-# get eras that are ongoing
+from scraper.db import db_cursor
+
+
+def get_ongoing_era_names():
+    with db_cursor() as cur:
+        cur.execute("SELECT name FROM eras WHERE ongoing = TRUE;")
+        return [row[0] for row in cur.fetchall()]
+
+
 # download(ongoing_eras)
 # data = scraper()
 # update sets and cards
